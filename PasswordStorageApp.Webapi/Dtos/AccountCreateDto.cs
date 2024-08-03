88ed60a28130cff23db697f126f630c0a1c7646f
savasduzgun <savasdzgn@gmail.com>
2024-08-03 12:37:16 +0300

    Dtos eklendi.

---
+++ PasswordStorageApp.Webapi/Dtos/AccountCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordStorageApp.Webapi.Dtos
+{
+    public class AccountCreateDto
+    {
+        [Required, MinLength(6),MaxLength(40)]
+        public string Username { get; set; }
+
+        [Required, MinLength(6), MaxLength(40)]
+        public string Password { get; set; }
+    }
+}
