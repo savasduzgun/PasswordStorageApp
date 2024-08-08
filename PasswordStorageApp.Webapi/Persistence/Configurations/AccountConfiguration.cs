06abceb19b0144a9671dae46c2fb0d8080e9bb69
savasduzgun <savasdzgn@gmail.com>
2024-08-08 16:07:07 +0300

    Configuration eklendi.

---
+++ PasswordStorageApp.Webapi/Persistence/Configurations/AccountConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PasswordStorageApp.Webapi.Models;
+
+namespace PasswordStorageApp.Webapi.Persistence.Configurations
+{
+    public class AccountConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
