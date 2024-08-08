37917a0f544c7e2733c51de07a03ccaeb7bea732
savasduzgun <savasdzgn@gmail.com>
2024-08-08 16:30:26 +0300

    DbContext e bildirildi.

--- PasswordStorageApp.Webapi/Persistence/Contexts/ApplicationDbContext.cs
+++ PasswordStorageApp.Webapi/Persistence/Contexts/ApplicationDbContext.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PasswordStorageApp.Webapi.Models;
+using PasswordStorageApp.Webapi.Persistence.Configurations;
 
 namespace PasswordStorageApp.Webapi.Persistence.Contexts
 {
     public class ApplicationDbContext :DbContext
     {
         public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.ApplyConfiguration(new AccountConfiguration());
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
     }
 }
